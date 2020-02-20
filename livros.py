5bf2540726a8e786fc031695d7aa00c550c00288
Pedro Condeco <pedro.condeco@freiheit.com>
2020-02-20 20:06:18 +0000

    dicas

--- livros.py
+++ livros.py
@@ -7,49 +7,43 @@ class Book:
 
 
 class Lib:
-    def __init__(self, books, signup_days, books_per_day):
+    def __init__(self, books, warmup, books_per_day, id):
+        self.id = id
         self.total_books = len(books)
-        self.signup_days = signup_days
+        self.warmup = warmup
         self.books_per_day = books_per_day
-        self.books = books
-        self.choosen = False
-        self.scanning = False
+        self.bookIndex = 0
+        self.books = sorted(books, key=lambda x: x.rating, reverse=True)
+        self.day = -1
+        self.value = totalValue(books, books_per_day, warmup)
+        
+        
+    def shipBooks(self):
+        totalScanningDays = TOTAL_DAYS - self.day - self.warmup
+        totalBooks = min(totalScanningDays * self.books_per_day, len(self.books)) 
+        return self.books[0:totalBooks-1]
 
 
 
-    def totalValue(self):
-        value = 0
-        for b in self.books:
-            value += BOOK_SCORES[b]
-
-        self.Valeu =  value/(len(self.book_ids)/self.books_per_day + self.signup_days)
-
-    def tick(self):
+def totalValue(books, books_per_day,warmup):
+    value = 0
+    for b in books:
+        value += b.rating
+    return  value/(len(books)/books_per_day + warmup)
 
 
-        return        
-
-
-
-
-    
-
-
-
-def chooseLib(): 
-    best = 0 
-    escolhida  = Lib(1,1,1)
-    for l in LIBRARIES:
-        if not l.Valeu: 
-            continue
-        if l.totalValue() > best:
-            best = l.Valeu
-            escolhida = l
+def bestLib(libs, day):
+    best = 0
+    lib = Lib(1,1,1,-1)
+    for l in libs:
+        if best < lib.value:
+            best = lib.value
+            lib = l
+    lib.day = day    
+    return lib
 
 
 
-    escolhida.alive = False
-    return best
 
 
 
@@ -60,14 +54,48 @@ TOTAL_LIBS = 0
 TOTAL_DAYS = 0
 BOOK_SCORES = []
 LIBRARIES = []
-with open("a_example.txt", "r") as a:
-    TOTAL_BOOKS, TOTAL_LIBS, TOTAL_DAYS = list(map(int, a.readline().split(" ")))
-    BOOK_SCORES = list(map(int, a.readline().split(" ")))
-    for _ in range(TOTAL_LIBS):
-        total_books, signup_days, books_per_day = list(map(int, a.readline().split(" ")))
-        lib = Lib(total_books, signup_days, books_per_day)
-        lib.book_ids = set(map(int, a.readline().split(" ")))
-        LIBRARIES.append(lib)
-print(TOTAL_BOOKS, TOTAL_LIBS, TOTAL_DAYS)
-print(BOOK_SCORES)
-print(LIBRARIES)
+BOOK_IDS_PER_LIB = []
+LIBS_TO_SIGNUP = []
+TOTAL_BOOKS_PER_LIB = []
+
+
+
+def writeln(out, text):
+    out.write(str(text))
+    out.write("\n")
+
+def write_output(lib_ids, total_books_per_lib, book_ids_per_lib):
+    with open("output.txt", "w") as out:
+        writeln(out, len(lib_ids))
+        writeln(out, " ".join(map(str, lib_ids)))
+        for i in range(len(lib_ids)):
+            writeln(out, str(lib_ids[i]) + " " + str(total_books_per_lib[i]))
+            writeln(out, " ".join(map(str, book_ids_per_lib[i])))
+
+
+
+
+    
+if __name__ == "__main__":
+    with open("a_example.txt", "r") as a:
+        TOTAL_BOOKS, TOTAL_LIBS, TOTAL_DAYS = list(map(int, a.readline().split(" ")))
+        BOOK_SCORES = list(map(int, a.readline().split(" ")))
+        for id in range(TOTAL_LIBS):
+            total_books, warmup, books_per_day = list(map(int, a.readline().split(" ")))
+            book_ids = set(map(int, a.readline().split(" ")))
+            books = [Book(id, BOOK_SCORES[id]) for id in book_ids]
+            lib = Lib(books, warmup, books_per_day, id)
+            LIBRARIES.append(lib)
+
+    books_per_lib = []
+    DAY = 0
+    while DAY < TOTAL_DAYS :
+        bestLib = bestLib(LIBRARIES, DAY)
+        books = bestLib.shipBooks()
+        BOOK_IDS_PER_LIB.append(books) 
+        TOTAL_BOOKS_PER_LIB.append(len(books))
+        LIBS_TO_SIGNUP.append(bestLib.id)
+        DAY += bestLib.warmup
+        
+        
+    write_output(LIBS_TO_SIGNUP, TOTAL_BOOKS_PER_LIB, BOOK_IDS_PER_LIB)
